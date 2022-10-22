2f0eb59b572fff99e525fb83abbc1339fe277055
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-10-22 15:42:35 +0800

    feat: add stargen functions...
    - system name generator
    - planet generator
    - star generator

---
+++ src/stargen.lua
@@ -0,0 +1,67 @@
+local math = require("math")
+local json = require("json")
+local data = require("seeddata")
+local util = require("util")
+
+local p = {}
+
+function p.genSystemName()
+    local name = ""
+    local possibleShards = data.nameShards
+    local shardsCount = math.random(2, 5)
+
+    for i=1,shardsCount do
+        name = name .. possibleShards[math.random(#possibleShards)]
+    end
+
+    return util.titleCase(name)
+end
+
+function p.genStar()
+    local types = data.stars.possibleTypes
+
+    local star = {
+        type = types[math.random(#types)]
+    }
+    return star;
+end
+
+function p.genPlanet()
+    local types = data.planets.possibleTypes
+
+    local planet = {
+        type = types[math.random(#types)],
+        population = math.random(1,800000)*100
+    }
+
+    return planet;
+end
+
+function p.main(args)
+    local system = {
+        metadata = {
+            generated = os.date("%Y-%m-%d %H:%M:%S",os.time()),
+            randseed = util.state.seed
+        },
+        name = p.genSystemName(),
+        star = p.genStar(),
+        planets = {}
+    }
+
+    local planetCount = math.random(2,9)
+
+    for i=1,planetCount do
+        system.planets[i] = p.genPlanet()
+
+        local i_str = tostring(i)
+        system.planets[i].name = system.name .. " " .. i_str
+    end
+
+    if args.json then
+        print(json.encode(system))
+    else
+        
+    end
+end
+
+return p
