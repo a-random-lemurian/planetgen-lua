9cb5e172acf47b0dc61ecb448f6a476ee5bc53d6
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-10-22 15:38:46 +0800

    feat: add seed data

---
+++ src/seeddata.lua
@@ -0,0 +1,28 @@
+local data = {
+    planets = {
+        possibleTypes = {
+            "terran",
+            "barren",
+            "lava",
+            "venusian"
+        }
+    },
+    stars = {
+        possibleTypes = {
+            "red",
+            "blue",
+            "yellow",
+            "orange",
+            "white",
+            "blackhole",
+            "neutron"
+        },
+    },
+    nameShards = {
+        "sh", "ca", "ma", "lop", "nar", "le", "mel", "ral", "dan",
+        "kam", "kai", "ai", "ei", "ro", "de", "ze", "ya", "ik", "vot",
+        "sen"
+    }
+}
+
+return data;
