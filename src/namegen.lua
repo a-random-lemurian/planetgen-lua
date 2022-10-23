c6d8bb3262ac99b416a9c962f7ed206b0bda16b1
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-10-23 16:32:42 +0800

    feat(names): add katakana name provider

--- src/namegen.lua
+++ src/namegen.lua
@@ -2,8 +2,59 @@ local data = require("seeddata")
 local util = require("util")
 local p = {}
 
+local katakana = {
+    startFragments = {
+        "kyo", "gyo", "shu", "sho", "chu", "cho", "hyu", "myo",
+        "ryu", "chi", "tsu", "shi", "ka", "ki", "ku", "ke",
+        "ko", "ga", "gi", "gu", "ge", "go", "sa", "su",
+        "se", "so", "za", "zu", "ze", "ta", "te", "to",
+        "da", "na", "ni", "nu", "ne", "no", "ha", "hi",
+        "fu", "he", "ho", "ba", "bi", "bu", "be", "ma",
+        "mi", "mu", "me", "mo", "ya", "yu", "yo", "ri",
+        "ru", "wa", "jo", "a", "i", "u", "e", "o"
+    },
+    middleFragments = {
+        "sshi", "ppo", "tto", "mbo", "kka", "kyu", "sho", "chu",
+        "chi", "tsu", "shi", "ka", "ki", "ku", "ke", "ko",
+        "ga", "gi", "gu", "ge", "go", "sa", "su", "se",
+        "so", "za", "ji", "zu", "ze", "ta", "te", "to",
+        "da", "de", "do", "na", "ni", "nu", "ne", "no",
+        "ha", "hi", "fu", "ho", "ba", "bi", "bu", "be",
+        "bo", "ma", "mi", "mu", "me", "mo", "ya", "yo",
+        "ra", "ri", "ru", "re", "ro", "wa", "ju", "jo",
+        "a", "i", "u", "e", "o", "n"
+    },
+    endFragments = {
+        "ttsu", "ppu", "ssa", "tto", "tte", "noh", "mba", "kko",
+        "kyo", "shu", "chu", "nyu", "nyo", "ryu", "chi", "tsu",
+        "shi", "ka", "ki", "ku", "ke", "ko", "ga", "gi",
+        "gu", "go", "sa", "su", "se", "so", "za", "ji",
+        "zu", "ze", "zo", "ta", "te", "to", "da", "de",
+        "do", "na", "ni", "ne", "no", "ha", "hi", "fu",
+        "he", "ho", "ba", "bi", "bu", "be", "bo", "ma",
+        "mi", "mu", "me", "mo", "ya", "yo", "ra", "ri",
+        "ru", "re", "ro", "wa", "ja", "jo", "i", "e",
+        "o", "n"
+    }
+}
+
 p.nameProviders = {
 }
+
+function p.nameProviders.katakana()
+    local name = ""
+    local middleFragmentCount = math.random(0, 2)
+    
+    name = name .. util.randomItemFromList(katakana.startFragments)
+    
+    for i=0,middleFragmentCount do
+        name = name .. util.randomItemFromList(katakana.middleFragments)
+    end
+
+    name = name .. util.randomItemFromList(katakana.endFragments)
+
+    return name
+end
 
 function p.nameProviders.generic()
     local name = ""
