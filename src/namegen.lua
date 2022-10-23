b4bd8ff8f4c35eb4182341112d335feef6ae77ae
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-10-23 16:33:51 +0800

    refactor: expand scope of minShards and maxShards to file

--- src/namegen.lua
+++ src/namegen.lua
@@ -2,6 +2,9 @@ local data = require("seeddata")
 local util = require("util")
 local p = {}
 
+local minShards = util.overrideArgument("min_name_shards", data.defaults.minNameShards);
+local maxShards = util.overrideArgument("max_name_shards", data.defaults.maxNameShards);
+
 local katakana = {
     startFragments = {
         "kyo", "gyo", "shu", "sho", "chu", "cho", "hyu", "myo",
@@ -59,10 +62,6 @@ end
 function p.nameProviders.generic()
     local name = ""
     local possibleShards = data.nameShards
-
-    -- TODO: minimize code duplication with new function
-    local minShards = util.overrideArgument("min_name_shards", data.defaults.minNameShards);
-    local maxShards = util.overrideArgument("max_name_shards", data.defaults.maxNameShards);
 
     local shardsCount = math.random(minShards, maxShards)
 
